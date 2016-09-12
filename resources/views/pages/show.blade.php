14da31a520a192b2f09e90407e5ff0844eb99700
danixoid <danixoid@gmail.com>
2016-09-12 22:18:26 +0600

    some fixes

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -37,3 +37,36 @@
     </div>
 
 @endsection
+
+@section('javascript')
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+
+                <div class="modal-body balloon right">
+                    <img />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script language="javascript">
+
+        $(function(){
+
+            $('.well img').click(function(e) {
+
+                var src = $(this).attr('src');
+
+                $('#exampleModal').modal('toggle');
+                $('#exampleModal .modal-body img').attr('src',src);
+            });
+        });
+
+
+    </script>
+@endsection
