f3aef58541300e4c0787578b4c998cfb3f93b0a1
danixoid <danixoid@gmail.com>
2016-09-12 22:44:59 +0600

    some fixes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -57,6 +57,9 @@
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
 
                 <div class="modal-body">
                     <img />
