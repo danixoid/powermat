7e11ad37416497c85f85e511a069f5829e4d1206
danixoid <danixoid@gmail.com>
2016-09-12 21:16:25 +0600

    some fixes

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -147,13 +147,16 @@ class LocationController extends Controller
         $logo_file = $request->file('logo_file');
         $img_file = $request->file('img_file');
 
-        if($logo_file && in_array(strtolower($img_file->getClientOriginalExtension()),$extentions)
+        if($logo_file
+            //&& in_array(strtolower($img_file->getClientOriginalExtension()),$extentions)
         ) {
             $logo_file->move(storage_path('app/'));
             $data['logo'] = $logo_file->getFilename();
         }
 
-        if($img_file && in_array(strtolower($img_file->getClientOriginalExtension()),$extentions)) {
+        if($img_file
+            //&& in_array(strtolower($img_file->getClientOriginalExtension()),$extentions)
+        ) {
             $img_file->move(storage_path('app/'));
             $data['img'] = $img_file->getFilename();
         }
